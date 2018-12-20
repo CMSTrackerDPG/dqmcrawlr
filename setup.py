74bb7d189e28debb6abbad8be4b3042cad00a866
Peter Stein <peterstein@protonmail.com>
2018-12-20 18:25:13 +0100

    Bump version 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dqmcrawlr",
-    version="0.3.2",
+    version="0.4.0",
     desription="CERN CMS Data Quality Monitor JSON crawler.",
     url="https://github.com/ptrstn/dqmcrawlr",
     author="Peter Stein",
